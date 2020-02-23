3ef9b023224b1edb46aad7b42f5247c0982d4cd2
d4rkr41n <you@example.com>
2020-02-22 19:56:33 -0600

    Added csv writing feature

--- all.py
+++ all.py
@@ -1,3 +1,4 @@
+import csv
 import numpy as np
 import pandas as pd
 #import seaborn as sb
@@ -12,17 +13,21 @@ from sklearn import metrics
 training = pd.read_csv('UNSW_NB15_training-set.csv')
 testing = pd.read_csv('UNSW_NB15_testing-set.csv')
 
-#print(training.head())
-#print(training.corr())
-
+# This mess is where you can play with different features
 train_x = training.loc[:, ['sttl', 'dmean', 'rate', 'dwin', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'swin', 'dload']]
-#train_x = training.loc[:, ['sttl', 'rate', 'dur']]
-#train_x = training.loc[:, ['sttl', 'dmean', 'rate', 'dwin', 'swin', 'dload']]
 test_x = testing.loc[:, ['sttl', 'dmean', 'rate', 'dwin', 'ct_src_dport_ltm', 'ct_dst_sport_ltm', 'swin', 'dload']]
-#test_x = testing.loc[:, ['sttl', 'rate', 'dur']]
+
+#train_x = training.loc[:, ['sttl', 'dmean', 'rate', 'dwin', 'swin', 'dload']]
 #test_x = testing.loc[:, ['sttl', 'dmean', 'rate', 'dwin', 'swin', 'dload']]
+#test_x = testing.loc[:, ['sttl', 'rate', 'dur']]
+#train_x = training.loc[:, ['sttl', 'rate', 'dur']]
+
+
+
+# These will pretty much stay the same
 train_y = training.loc[:,['label']]
 test_y = testing.loc[:,['label']]
+
 
 def tree():
     print("--------------------------------")
@@ -36,11 +41,16 @@ def tree():
     print("Train: %s" % model.score(train_x, train_y.values.ravel()))
     predictions = model.predict(test_x)
 
+    # Write Preditions to CSV file
+    report_csv('Result-DecisionTree.csv', predictions)
+
+    # Test out the model on the training data
     trainpred = model.predict(train_x)
     tcm = metrics.confusion_matrix(train_y, trainpred)
     print("\nCM - Training Model")
     print(tcm)
 
+    # Test out the model on the testing data
     print("\nCM - Testing Model")
     cm = metrics.confusion_matrix(test_y, predictions)
     print(cm)
@@ -57,13 +67,17 @@ def KNN():
     print("Train: %s" % model.score(train_x, train_y.values.ravel()))
     predictions = model.predict(test_x)
 
+    # Write Preditions to CSV file
+    report_csv('Result-KNN.csv', predictions)
+
+    # Test out the model on the training data
     trainpred = model.predict(train_x)
     tcm = metrics.confusion_matrix(train_y, trainpred)
     print("\nCM - Training Model")
     print(tcm)
 
+    # Test out the model on the testing data
     print("\nCM - Testing Model")
-
     cm = metrics.confusion_matrix(test_y, predictions)
     print(cm)
 
@@ -79,17 +93,26 @@ def LR():
     print("Train: %s" % model.score(train_x, train_y.values.ravel()))
     predictions = model.predict(test_x)
 
+    # Write Preditions to CSV file
+    report_csv('Result-LogisticRegression.csv', predictions)
+
+    # Test out the model on the training data
     trainpred = model.predict(train_x)
     tcm = metrics.confusion_matrix(train_y, trainpred)
     print("\nCM - Training Model")
     print(tcm)
 
+    # Test out the model on the testing data
     print("\nCM - Testing Model")
-
     cm = metrics.confusion_matrix(test_y, predictions)
     print(cm)
 
+def report_csv(csvname, data):
+    with open(csvname, mode='w') as file:
+        file_writer = csv.writer(file, delimiter=',', quotechar='"', quoting=csv.QUOTE_MINIMAL)
+        file_writer.writerow(data)
+
 if __name__ == "__main__":
     tree()
-    KNN()
-    LR()
+    #KNN()
+    #LR()
